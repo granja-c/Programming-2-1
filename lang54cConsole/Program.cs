dd938298acf283f6de4760d78b104877cb539e04
granja-c <112727067+granja-c@users.noreply.github.com>
2023-04-04 08:37:12 -0500

    f

---
+++ lang54cConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lang54cConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the diameter:");
+            int dia = int.Parse(Console.ReadLine());
+        }
+    }
+}
