579b4cbb19133ffa052432baf3fda05ed95ba1b6
granja-c <112727067+granja-c@users.noreply.github.com>
2023-04-04 08:28:15 -0500

    f

---
+++ favclubConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace favclubConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("My favorite club is French Club. ");
+        }
+    }
+}
---
+++ yournameConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yournameConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("My name is Camila Mae Granja.")
+        }
+    }
+}
