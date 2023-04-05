aee701a05c4ed3595ac3ed8447772ff9f8cb6651
granja-c <112727067+granja-c@users.noreply.github.com>
2023-04-05 08:23:50 -0500

    update

--- lang54cConsole/Program.cs
+++ lang54cConsole/Program.cs
@@ -11,9 +11,9 @@ namespace lang54cConsole
         static void Main(string[] args)
         {
             Console.Write("Enter the diameter:");
-            int dia = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
             double pi = 3.14159;
-            double rad = dia / 2.0;
+            double rad = d / 2.0;
             double area = rad * rad * pi;
             double circ = pi * rad * 2;
 
